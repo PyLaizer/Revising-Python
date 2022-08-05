f166616b0d7703c055c8bfae4c660317576f6948
Edward Eleazar <laizer2100@gmail.com>
2022-08-05 19:14:30 +0100

    some changes

---
+++ Python/Crash_course_exercises/Chapter2.py
@@ -0,0 +1,40 @@
+# Exercise 2.3 to 2.7
+
+#2.3
+a = "Jonas"
+print(f"Hello {a}, would you like some tea")
+
+#2.4
+b = "Martha Nielsen"
+print(b.title())
+print(b.lower())
+print(b.upper())
+
+#2.5
+person = "Albert Einstein"
+quote = '"A person who never made a mistake never tried anything new."'
+print(f"{person} once said, {quote}")
+
+#2.6
+famous_person = "Albert Einstein"
+message = f'{famous_person} once said, "A person who never made a mistake never tried anything new."  '
+print(message)
+
+#2.7
+father = " Micheal "
+print(father)
+print(father.lstrip())
+print(father.rstrip())
+print(father.strip())
+
+
+#2.8
+print(6 + 2)
+print(19-11)
+print(2 * 4)
+print(32 / 4)
+
+#2.9
+my_no = 10
+message2 = f"My favourite number is {my_no}"
+print(message2)
